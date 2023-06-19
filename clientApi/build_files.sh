08633e63b5b1a6c1c18f8667c32cb093581b7e4a
Santiago Cifuentes <cifuentess938@gmail.com>
2023-06-19 03:50:17 -0500

    u 2

--- clientApi/build_files.sh
+++ clientApi/build_files.sh
@@ -1,6 +1,7 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
 python3.9 setup.py build_ext --pg-config /path/to/pg_config build
+python3.9 -m pip install -r requirements.txt
+
 python3.9 manage.py collectstactic --noinput --clear
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput

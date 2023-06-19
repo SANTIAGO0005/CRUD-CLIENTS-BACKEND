477d95868f87aab8a023480c46e7cc1d83efdfb2
Santiago Cifuentes <cifuentess938@gmail.com>
2023-06-19 04:39:45 -0500

    resolved all

--- clientApi/build_files.sh
+++ clientApi/build_files.sh
@@ -1,7 +1,7 @@
 echo "BUILD START"
-
+python3.9 pip install pipwin
+python3.9 pipwin install psycopg2==2.9.6
 python3.9 -m pip install -r requirements.txt
-
 python3.9 manage.py collectstactic --noinput --clear
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput

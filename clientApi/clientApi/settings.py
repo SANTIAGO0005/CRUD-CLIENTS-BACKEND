e6ac11e5072147710a000b19ede4432c61ffde8d
Santiago Cifuentes <cifuentess938@gmail.com>
2023-06-19 12:59:22 -0500

    add origin white

--- clientApi/clientApi/settings.py
+++ clientApi/clientApi/settings.py
@@ -30,15 +30,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
     'rest_framework',
     'clients',
-
-     'corsheaders',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -46,8 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'clientApi.urls'
@@ -147,14 +146,15 @@ API_DESCRIPTION = 'API for managing clients'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'https://crud-clients-frontend.vercel.app',
-    'http://client-api-five.vercel.app/',
+    'http://client-api-five.vercel.app',
     'http://localhost:3000',
     'http://192.168.100.63:3000'
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://crud-clients-frontend.vercel.app',
+CORS_ORIGIN_WHITELIST = [
+    'https://crud-clients-frontend.vercel.app'
 ]
+
 
 CORS_ALLOW_METHODS = (
     "DELETE",

ba1caf515c63ad81f5daf19c0297548f6a2cf9fd
Santiago Cifuentes <cifuentess938@gmail.com>
2023-06-19 04:08:53 -0500

    resolved errors

--- clientApi/build_files.sh
+++ clientApi/build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.9 setup.py build_ext --pg-config /path/to/pg_config build
+
 python3.9 -m pip install -r requirements.txt
 
 python3.9 manage.py collectstactic --noinput --clear

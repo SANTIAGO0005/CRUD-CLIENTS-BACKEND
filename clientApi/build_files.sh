0e3946f4ba376836b4ac1bc69748d85abe9d74e1
Santiago Cifuentes <cifuentess938@gmail.com>
2023-06-19 11:36:03 -0500

    resolved finally

--- clientApi/build_files.sh
+++ clientApi/build_files.sh
@@ -1,7 +1,4 @@
 echo "BUILD START"
-winget install postqresql
-python3.9 pip install pipwin
-python3.9 pipwin install psycopg2==2.9.6
 python3.9 manage.py collectstactic --noinput --clear
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput

db66def7d6dd65eaae76a7da44fbecc3091fef03
Santiago Cifuentes <cifuentess938@gmail.com>
2023-06-19 22:01:37 -0500

    update build

--- clientApi/build_files.sh
+++ clientApi/build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.9 -m pip install django-cors-headers
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstactic --noinput --clear
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
